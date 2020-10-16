fff2b243a1f6876b54230860d5ef5df4cbb10cb9
trancongduynguyen1997 <tcdnguyen1997@gmail.com>
2020-10-16 08:19:27 +0700

    Implement interfaces for services. Closes #5

---
+++ data/src/main/java/nguyen/guru/petclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package nguyen.guru.petclinic.services;
+
+import nguyen.guru.petclinic.models.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ data/src/main/java/nguyen/guru/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package nguyen.guru.petclinic.services;
+
+import nguyen.guru.petclinic.models.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
